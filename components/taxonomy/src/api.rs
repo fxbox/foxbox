37b08040fb6e265816b660706ada0b2fc1252890
David Rajchenbach-Teller <david.teller@univ-orleans.fr>
2016-02-24 10:53:52 +0100

    [fxbox/taxonomy] Refining API

--- components/taxonomy/src/api.rs
+++ components/taxonomy/src/api.rs
@@ -54,7 +54,11 @@ pub enum WatchEvent {
 
 /// The public API.
 pub trait API {
-    /// Get a list of nodes matching some conditions
+    /// Get the metadata on nodes matching some conditions.
+    ///
+    /// A call to `API::get_nodes(vec![req1, req2, ...])` will return
+    /// the metadata on all nodes matching _either_ `req1` or `req2`
+    /// or ...
     ///
     /// # REST API
     ///
@@ -62,7 +66,7 @@ pub trait API {
     ///
     /// ## Inputs
     ///
-    /// Any JSON that can be deserialized to a `NodeRequest`. See
+    /// Any JSON that can be deserialized to a `Vec<NodeRequest>`. See
     /// the implementation of `NodeRequest` for details.
     ///
     /// ### Example
@@ -71,7 +75,7 @@ pub trait API {
     /// that support output service `OpenClose`
     ///
     /// ```json
-    /// {
+    /// [{
     ///   "tags": ["entrance", "door"],
     ///   "inputs": [
     ///     {
@@ -82,14 +86,14 @@ pub trait API {
     ///       }
     ///     }
     ///   ]
-    /// }
+    /// }]
     /// ```
     ///
     ///
     /// ## Errors
     ///
-    /// In case of syntax error, Error 400, accompanied with a JSON
-    /// string detailing the error.
+    /// In case of syntax error, Error 400, accompanied with a
+    /// somewhat human-readable JSON string detailing the error.
     ///
     /// ## Success
     ///
@@ -99,7 +103,7 @@ pub trait API {
     /// ### Example
     ///
     /// ```json
-    /// {
+    /// [{
     ///   "tags": ["entrance", "door", "somevendor"],
     ///   "id: "some-node-id",
     ///   "inputs": [],
@@ -118,75 +122,210 @@ pub trait API {
     ///       }
     ///     }
     ///   ]
+    /// }]
+    /// ```
+    fn get_nodes(&Vec<NodeRequest>) -> Vec<Node>;
+
+    /// Label a set of nodes with a set of tags.
+    ///
+    /// A call to `API::put_node_tag(vec![req1, req2, ...], vec![tag1,
+    /// ...])` will label all the nodes matching _either_ `req1` or
+    /// `req2` or ... with `tag1`, ... and return the number of nodes
+    /// matching any of the requests.
+    ///
+    /// Some of the nodes may already be labelled with `tag1`, or
+    /// `tag2`, ... They will not change state. They are counted in
+    /// the resulting `usize` nevertheless.
+    ///
+    /// Note that this call is _not live_. In other words, if nodes
+    /// are added after the call, they will not be affected.
+    ///
+    /// # REST API
+    ///
+    /// `PUT /api/v1/node/tag`
+    ///
+    /// ## Inputs
+    ///
+    /// Any JSON that can be deserialized to
+    ///
+    /// ```rust
+    /// {
+    ///   set: Vec<NodeRequest>,
+    ///   tags: Vec<String>,
     /// }
     /// ```
-    fn get_nodes(&NodeRequest) -> Vec<Node>;
-
-    /// Add a tag to an existing node.
     ///
-    /// Tags can be used to locate nodes.
+    /// ## Errors
+    ///
+    /// In case of syntax error, Error 400, accompanied with a
+    /// somewhat human-readable JSON string detailing the error.
+    ///
+    /// ## Success
+    ///
+    /// A JSON representing a number.
+    fn put_node_tag(set: &Vec<NodeRequest>, tags: &Vec<String>) -> usize;
+
+    /// Remove a set of tags from a set of nodes.
+    ///
+    /// A call to `API::delete_node_tag(vec![req1, req2, ...], vec![tag1,
+    /// ...])` will remove from all the nodes matching _either_ `req1` or
+    /// `req2` or ... all of the tags `tag1`, ... and return the number of nodes
+    /// matching any of the requests.
+    ///
+    /// Some of the nodes may not be labelled with `tag1`, or `tag2`,
+    /// ... They will not change state. They are counted in the
+    /// resulting `usize` nevertheless.
+    ///
+    /// Note that this call is _not live_. In other words, if nodes
+    /// are added after the call, they will not be affected.
     ///
     /// # REST API
     ///
-    /// `PUT /api/v1/node/tag/$NodeId`
-    fn put_node_tag(&NodeId, String) -> Result<(), Error>;
-
-    /// Add a tag to an existing node.
+    /// `DELETE /api/v1/node/tag`
     ///
-    /// Tags can be used to locate nodes.
+    /// ## Inputs
     ///
-    /// # REST API
+    /// Any JSON that can be deserialized to
     ///
-    /// `DELETE /api/v1/node/tag/$NodeId`
-    fn delete_node_tag(&NodeId, String) -> Result<(), Error>;
+    /// ```rust
+    /// {
+    ///   set: Vec<NodeRequest>,
+    ///   tags: Vec<String>,
+    /// }
+    /// ```
+    ///
+    /// ## Errors
+    ///
+    /// In case of syntax error, Error 400, accompanied with a
+    /// somewhat human-readable JSON string detailing the error.
+    ///
+    /// ## Success
+    ///
+    /// A JSON representing a number.
+    fn delete_node_tag(set: &Vec<NodeRequest>, tags: String) -> usize;
     
     /// Get a list of inputs matching some conditions
     ///
     /// # REST API
     ///
     /// `GET /api/v1/service/list`
-    fn get_input_services(&InputRequest) -> Vec<Service<Input>>;
-    fn get_output_services(&OutputRequest) -> Vec<Service<Output>>;
+    fn get_input_services(&Vec<InputRequest>) -> Vec<Service<Input>>;
+    fn get_output_services(&Vec<OutputRequest>) -> Vec<Service<Output>>;
 
-    /// Add a tag to an existing service.
+    /// Label a set of services with a set of tags.
     ///
-    /// Tags can be used to locate service.
+    /// A call to `API::put_{input, output}_tag(vec![req1, req2, ...], vec![tag1,
+    /// ...])` will label all the services matching _either_ `req1` or
+    /// `req2` or ... with `tag1`, ... and return the number of services
+    /// matching any of the requests.
+    ///
+    /// Some of the services may already be labelled with `tag1`, or
+    /// `tag2`, ... They will not change state. They are counted in
+    /// the resulting `usize` nevertheless.
+    ///
+    /// Note that this call is _not live_. In other words, if services
+    /// are added after the call, they will not be affected.
     ///
     /// # REST API
     ///
-    /// `PUT /api/v1/service/tag/$ServiceId`
-    fn put_service_tag(&ServiceId, String) -> Result<(), Error>;
-
-    /// Add a tag to an existing service.
+    /// `PUT /api/v1/service/tag`
     ///
-    /// Tags can be used to locate services.
+    /// ## Inputs
+    ///
+    /// Any JSON that can be deserialized to
+    ///
+    /// ```rust
+    /// {
+    ///   set: Vec<InputRequest>,
+    ///   tags: Vec<String>,
+    /// }
+    /// ```
+    /// or
+    /// ```rust
+    /// {
+    ///   set: Vec<OutputRequest>,
+    ///   tags: Vec<String>,
+    /// }
+    /// ```
+    ///
+    /// ## Errors
+    ///
+    /// In case of syntax error, Error 400, accompanied with a
+    /// somewhat human-readable JSON string detailing the error.
+    ///
+    /// ## Success
+    ///
+    /// A JSON representing a number.
+    fn put_input_tag(&Vec<InputRequest>, &Vec<String>) -> usize;
+    fn put_output_tag(&Vec<OutputRequest>, &Vec<String>) -> usize;
+
+    /// Remove a set of tags from a set of services.
+    ///
+    /// A call to `API::delete_{input, output}_tag(vec![req1, req2, ...], vec![tag1,
+    /// ...])` will remove from all the services matching _either_ `req1` or
+    /// `req2` or ... all of the tags `tag1`, ... and return the number of services
+    /// matching any of the requests.
+    ///
+    /// Some of the services may not be labelled with `tag1`, or `tag2`,
+    /// ... They will not change state. They are counted in the
+    /// resulting `usize` nevertheless.
+    ///
+    /// Note that this call is _not live_. In other words, if services
+    /// are added after the call, they will not be affected.
     ///
     /// # REST API
     ///
-    /// `DELETE /api/v1/service/tag/$ServiceId`
-    fn delete_service_tag(&ServiceId, String) -> Result<(), Error>;
+    /// `DELETE /api/v1/node/tag`
+    ///
+    /// ## Inputs
+    ///
+    /// Any JSON that can be deserialized to
+    ///
+    /// ```rust
+    /// {
+    ///   set: Vec<InputRequest>,
+    ///   tags: Vec<String>,
+    /// }
+    /// ```
+    /// or
+    /// ```rust
+    /// {
+    ///   set: Vec<OutputRequest>,
+    ///   tags: Vec<String>,
+    /// }
+    /// ```
+    ///
+    /// ## Errors
+    ///
+    /// In case of syntax error, Error 400, accompanied with a
+    /// somewhat human-readable JSON string detailing the error.
+    ///
+    /// ## Success
+    ///
+    /// A JSON representing a number.
+    fn delete_input_tag(&Vec<InputRequest>, &Vec<String>) -> usize;
+    fn delete_output_tag(&Vec<InputRequest>, &Vec<String>) -> usize;
 
-    /// Read one value from an input enpoint
+    /// Read the latest value from a set of services
     ///
     /// # REST API
     ///
-    /// GET /api/v1/service/value/$ServiceId
-    fn get_service_value(&Service<Input>) -> Result<Value, Error>;
+    /// `GET /api/v1/service/value`
+    fn get_service_value(&Vec<InputRequest>) -> Vec<(ServiceId, Result<Value, Error>)>;
 
-    /// Send one value to an output enpoint
+    /// Send one value to a set of services
     ///
     /// # REST API
     ///
-    /// `PUT /api/v1/service/value/$ServiceId`
-    fn put_service_value(&Service<Output>, Value) -> Result<(), Error>;
+    /// `PUT /api/v1/service/value`
+    fn put_service_value(&Vec<OutputRequest>, Value) -> Vec<(ServiceId, Result<(), Error>)>;
 
     /// Watch for any change
     ///
     /// # WebSocket API
     ///
-    /// `/api/v1/service/watch/$ServiceId`
-    fn register_service_watch<F>(WatchOptions, cb: F)
-                                 -> Result<Self::WatchGuard, Error>
+    /// `/api/v1/service/watch`
+    fn register_service_watch<F>(WatchOptions, cb: F) -> Self::WatchGuard
         where F: FnMut(WatchEvent) + Send + Sync;
 
     /// A value that causes a disconnection once it is dropped.
@@ -199,25 +338,46 @@ pub struct WatchOptions {
     /// The set of inputs to watch. Note that the actual inputs in the
     /// set may change over time.
     source: InputRequest,
+
+    /// If `true`, watch as new values become available.
+    should_watch_values: bool,
+
+    /// If `true`, watch as nodes are connected/disconnected.
+    should_watch_topology: bool,
 }
 
 impl WatchOptions {
     pub fn new() -> Self {
         WatchOptions {
             source: InputRequest::new(),
+            should_watch_values: false,
+            should_watch_topology: false,
         }
     }
 
-    /// Restrict to input services in a given set. Note that if all
-    /// the inputs do not have the same `ServiceKind`, the call to
-    /// `register_service_watch` will fail.
+    /// Restrict to input services in a given set.
     ///
     /// Also note that the actual input services that are part of the
     /// set may change with time, for instance if devices are added
-    /// ore removed.  as it changes.
+    /// ore removed.  The request _is live_, i.e. the service watch
+    /// will continue watching any input services that match `req`.
     pub fn with_inputs(self, req: InputRequest) -> Self {
         WatchOptions {
             source: self.source.and(req),
+            ..self
+        }
+    }
+
+    pub fn with_watch_values(self, should: bool) -> Self {
+        WatchOptions {
+            should_watch_values: should,
+            ..self
+        }
+    }
+
+    pub fn with_watch_topology(self, should: bool) -> Self {
+        WatchOptions {
+            should_watch_topology: should,
             ..self
         }
     }
