a777ff99b60ccfcbb011c4f489d1e11da0a19290
David Rajchenbach-Teller <david.teller@univ-orleans.fr>
2016-03-21 10:18:59 +0100

    [fxbox/thinkerbell] Trivial test tweak

--- components/thinkerbell/tests/test_rules.rs
+++ components/thinkerbell/tests/test_rules.rs
@@ -16,10 +16,12 @@ enum Event {
 }
 
 #[test]
-fn test_start() {
+fn test_compile() {
     let (tx, rx) : (_, Receiver<Event>)= channel();
 
     let tx_env = Box::new(tx.map(|event| Event::Simulator(event)));
+    let tx_simulator = tx.map(|event| Event::Env(event));
+
     let env = TestEnv::new(tx_env);
     let mut exec = Execution::<TestEnv>::new();
 
@@ -29,11 +31,18 @@ fn test_start() {
         }
     });
 
-    println!("* Attempting to parse an run an empty script...");
+    println!("* Attempting to parse an run an empty script will raise an error.");
     let script = Parser::parse("{\"rules\": []}".to_owned()).unwrap();
-    let tx_simulator = tx.map(|event| Event::Env(event));
     match exec.start(env, script, tx_simulator) {
         Err(Error::CompileError(CompileError::SourceError(SourceError::NoRule))) => {},
         other => panic!("Unexpected result {:?}", other)
     }
+
+    println!("//FIXME: Attempting to parse a script with an empty condition will raise an error.");
+    println!("//FIXME: Attempting to parse a script with an empty statement will raise an error.");
+    println!("//FIXME: Attempting to parse a script with an empty source will raise an error.");
+    println!("//FIXME: Attempting to parse a script with an empty destination will raise an error.");
+    println!("//FIXME: Attempting to parse a script with a type error in a match will raise an error.");
+    println!("//FIXME: Attempting to parse a script with a type error in a send will raise an error.");
+
 }
