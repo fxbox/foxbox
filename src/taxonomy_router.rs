f297ffbb907fc0d0e6651a10ce118f4238b98985
Fabrice Desré <fabrice@desre.org>
2016-03-24 09:21:40 -0700

    Rationalizing (de)serialization and error messages

--- src/taxonomy_router.rs
+++ src/taxonomy_router.rs
@@ -7,17 +7,22 @@ extern crate serde_json;
 use foxbox_taxonomy::manager::WatchGuard;
 use foxbox_taxonomy::api::{ API, TargetMap };
 use foxbox_taxonomy::values::Value;
-use foxbox_taxonomy::selector::{ GetterSelector, ServiceSelector, SetterSelector };
+use foxbox_taxonomy::selector::*;
 use foxbox_taxonomy::services::TagId;
 use foxbox_taxonomy::util::Id;
+
 use foxbox_users::AuthEndpoint;
+
 use iron::{ Handler, IronResult, Request, Response };
 use iron::headers::ContentType;
 use iron::method::Method;
 use iron::prelude::Chain;
+use iron::request::Body;
 use iron::status::Status;
+
 use serde::Serialize;
-use std::io::Read;
+
+use std::io::{ Error as IOError, Read };
 use std::sync::Mutex;
 use traits::Controller;
 
@@ -45,6 +50,12 @@ impl<A> TaxonomyRouter<A>
         response.status = Some(Status::Ok);
         response.headers.set(ContentType::json());
         Ok(response)
+    }
+
+    fn read_body_to_string<'a, 'b : 'a>(body: &mut Body<'a, 'b>) -> Result<String, IOError> {
+        let mut s = String::new();
+        try!(body.read_to_string(&mut s));
+        Ok(s)
     }
 }
 
@@ -80,12 +91,8 @@ impl<A> Handler for TaxonomyRouter<A>
                         // this kind of selector.
                         api.$call(vec![$sel::new()])
                     } else {
-                        // TODO: switch to serde_json::from_reader(req.body)
-                        let mut payload = String::new();
-                        itry!(req.body.read_to_string(&mut payload));
-                        let selectors: Vec<$sel> =
-                            itry!(serde_json::from_str(&payload));
-
+                        let source = itry!(Self::read_body_to_string(&mut req.body));
+                        let selectors = itry!(Vec::<$sel>::from_str(&source));
                         api.$call(selectors)
                     };
 
@@ -98,13 +105,11 @@ impl<A> Handler for TaxonomyRouter<A>
         macro_rules! payload_api {
             ($call:ident, $param:ty, $path:expr, $method:expr) => (
                 if path == $path && req.method == $method {
+                    type Selectors = $param;
                     return {
                         let api = self.api.lock().unwrap();
-                        let mut payload = String::new();
-                        itry!(req.body.read_to_string(&mut payload));
-                        let selectors: $param =
-                            itry!(serde_json::from_str(&payload));
-
+                        let source = itry!(Self::read_body_to_string(&mut req.body));
+                        let selectors = itry!(Selectors::from_str(&source));
                         let result = api.$call(selectors);
                         return self.build_response(&result);
                     }
@@ -117,19 +122,17 @@ impl<A> Handler for TaxonomyRouter<A>
         macro_rules! payload_api2 {
             ($call:ident, $name1:ident => $param1:ty, $name2:ident => $param2:ty, $path:expr, $method:expr) => (
                 if path == $path && req.method == $method {
+                    type Param1 = $param1;
+                    type Param2 = $param2;
                     return {
                         let api = self.api.lock().unwrap();
-                        let mut payload = String::new();
-                        itry!(req.body.read_to_string(&mut payload));
-                        #[derive(Deserialize)]
-                        struct Args {
-                            $name1: $param1,
-                            $name2: $param2
-                        }
-                        let args: Args =
-                            itry!(serde_json::from_str(&payload));
 
-                        let result = api.$call(args.$name1, args.$name2);
+                        let source = itry!(Self::read_body_to_string(&mut req.body));
+                        let mut args: JSON = itry!(serde_json::de::from_str(&source));
+                        let arg_1 = itry!(Param1::take(Path::new(), &mut args, stringify!($name1)));
+                        let arg_2 = itry!(Param2::take(Path::new(), &mut args, stringify!($name2)));
+
+                        let result = api.$call(arg_1, arg_2);
                         return self.build_response(&result);
                     }
                 }
@@ -259,7 +262,7 @@ describe! taxonomy_router {
     it "should return the list of services from a POST request" {
         let response = request::post("http://localhost:3000/api/v1/services",
                                     Headers::new(),
-                                    r#"[{"id":{"Exactly":"service:clock@link.mozilla.org"}}]"#,
+                                    r#"[{"id":"service:clock@link.mozilla.org"}]"#,
                                     &mount).unwrap();
         let body = response::extract_body_to_string(response);
         let observed: Vec<Service> = serde_json::from_str(&body).unwrap();
