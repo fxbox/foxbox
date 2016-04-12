821c492cdb144a0202f57a15389dc1a0d9c1db0f
Andrew Osmond <aosmond@mozilla.com>
2016-04-12 11:27:53 -0400

    Enable webpush on Mac, update instructions

--- tools/travis-osx.sh
+++ tools/travis-osx.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+export OPENSSL_LIB_DIR=/usr/local/opt/openssl/lib
+export EXTRA_LDFLAGS=-L/usr/local/opt/openssl/lib
+
 install_dependencies() {
     brew update
     brew install libupnp openssl sqlite
