d4a4c69665e5b496ef427ae30e24ec80bf08d611
No-Jun Park <npark@mozilla.com>
2016-04-27 08:47:14 -0400

    Add a new webpush test that utilizes the new web push standard with userAuth (#428)

--- test/integration/test/webpush_test.js
+++ test/integration/test/webpush_test.js
@@ -5,127 +5,187 @@ const chakram = require('chakram'), expect = chakram.expect;
 var Prepper = require('../lib/testPrepper.js');
 
 Prepper.makeSuite('Test Push Service locally',function(){
+  var pushURI = Prepper.webPush_server.getEndpointURI();
+  var pushkey = Prepper.webPush_server.getPublicKey();
+  
+  var baseSubscriptionPayload = {
+     'select': {
+       'id':'setter:subscribe.webpush@link.mozilla.org'
+     }, 
+     'value': {
+       'Json': {
+         'subscriptions':[{
+           'push_uri': Prepper.webPush_server.getEndpointURI(),
+           'public_key': Prepper.webPush_server.getPublicKey()
+         }]
+      }
+    }
+  };
+
+  var newWebPushSubscriptionPayload = 
+  Object.assign({}, baseSubscriptionPayload, {
+    value: {
+       Json: {
+          subscriptions: [{
+            'push_uri': Prepper.webPush_server.getEndpointURI(),
+            'public_key': Prepper.webPush_server.getPublicKey(),
+            'auth': Prepper.webPush_server.getUserAuth()
+          }]
+       }
+    }
+  });
+
+  var testParams = [{
+    suiteName: 'Old WebPush',
+    payload: baseSubscriptionPayload
+  }, 
+  {
+    suiteName: 'New WebPush',
+    payload: newWebPushSubscriptionPayload
+  }];
 
   before(Prepper.foxboxManager.foxboxLogin);
 
-  it('Check webpush service is registered',function(){  
-    var pushService = false;
+  testParams.forEach(testParam => {
+    describe(testParam.suiteName, function() {
 
-    return chakram.get(Prepper.foxboxManager.serviceListURL)
-    .then(function(listResponse) {
-      expect(listResponse).to.have.status(200);
-      for (var entry of listResponse.body) {
-        if (entry.adapter === 'webpush@link.mozilla.org') {
-          pushService = true;                
-        }
-      }
-      expect(pushService).equals(true);
-    });
-  });
+      it('Check webpush service is registered',function(){  
+        var pushService = false;
 
-  it('Add push subscription',function(){     
-    var pushURI = Prepper.webPush_server.getEndpointURI();
-    var pushkey = Prepper.webPush_server.getPublicKey();
-    var setter = 'setter:subscribe.webpush@link.mozilla.org';
-    var getter =  'getter:subscription.webpush@link.mozilla.org';
-    var setterPayload = {
-      'select': {
-        'id':setter
-      }, 
-      'value': {
-        'Json': {
-          'subscriptions':[{
-            'push_uri':pushURI,
-            'public_key':pushkey
-          }]}}};
-    var getterPayload = {'id': getter};    
-
-    return chakram.put(Prepper.foxboxManager.setterURL,setterPayload)
-    .then(function(cmdResp){
-     expect(cmdResp).to.have.status(200);
-     // When there is no error, the payload returns null with the setter
-     expect(cmdResp.body[setter]).equals(null);
-     return chakram.put(Prepper.foxboxManager.getterURL,getterPayload);
-    })
-    .then(function(cmdResp){
-      expect(cmdResp).to.have.status(200);
-      expect(cmdResp.body[getter].Json.subscriptions[0].public_key)
-      .equals(Prepper.webPush_server.getPublicKey());
-      expect(cmdResp.body[getter].Json.subscriptions[0].push_uri)
-      .equals(Prepper.webPush_server.getEndpointURI());
-    });
-  });
-
-  it('Set resources to receive notification',function(){     
-    var getter =  'getter:resource.webpush@link.mozilla.org';   
-    var setter = 'setter:resource.webpush@link.mozilla.org';
-    var setterPayload = {
-      'select': {
-        'id': setter}, 
-        'value': {
-          'Json': {
-            'resources':['livingroom', 'washroom']}}};
-    var getterPayload = {'id': getter};
-
-    return chakram.put(Prepper.foxboxManager.setterURL,setterPayload)
-    .then(function(cmdResp){
-     expect(cmdResp).to.have.status(200);
-     expect(cmdResp.body[setter]).equals(null);
-     return chakram.put(Prepper.foxboxManager.getterURL,getterPayload);
-    })
-    .then(function(cmdResp){
-      expect(cmdResp).to.have.status(200);
-      expect(cmdResp.body[getter]
-        .Json.resources[0]).equals('livingroom');
-      expect(cmdResp.body[getter]
-        .Json.resources[1]).equals('washroom');
-    });  
-  });
-
-  it('Accepts notification trigger requests',function(){
-    var setter = 'setter:notify.webpush@link.mozilla.org';
-    var resource = 'washroom';
-    var notificationText = 'lights on!';
-    var payload = {
-      'select': {
-        'id': setter
-      }, 
-      'value': {
-        'WebPushNotify': {
-          'resource':resource,'message':notificationText}}} ;
-    return chakram.put(Prepper.foxboxManager.setterURL, payload)
-    .then(function(cmdResponse) {
-      expect(cmdResponse).to.have.status(200);
-      expect(cmdResponse.body[setter]).equals(null);
-    });
-  });
-
-  describe ('Once notification is sent', function () {
-    var resource = 'livingroom';
-    var notificationText = 'can you encrypt this one too?';
-
-    before(function(done) {
-      var setter = 'setter:notify.webpush@link.mozilla.org';
-      var payload = {
-        'select': {
-          'id': setter
-        }, 
-        'value': {
-          'WebPushNotify': {
-            'resource':resource,'message':notificationText}}} ;
-      return chakram.put(Prepper.foxboxManager.setterURL, payload)
-      .then(function(cmdResponse) {
-      // collect the response from the webpush simulator 
-      // after 100ms of (network)latency
-      setTimeout(done,100);
+        return chakram.get(Prepper.foxboxManager.serviceListURL)
+        .then(function(listResponse) {
+          expect(listResponse).to.have.status(200);
+          for (var entry of listResponse.body) {
+            if (entry.adapter === 'webpush@link.mozilla.org') {
+              pushService = true;                
+            }
+          }
+          expect(pushService).equals(true);
+        });
       });
-    });
 
-    it('Check notification is received by Push server',function(){
-      var output = JSON.parse(Prepper.webPush_server.getDecodedPushMsg());
+      it('Add push subscription',function(){     
+        var setter = 'setter:subscribe.webpush@link.mozilla.org';
+        var getter =  'getter:subscription.webpush@link.mozilla.org';
+        // differs by the type of webpush std
+        var setterPayload = testParam.payload;  
+        var getterPayload = {'id': getter};    
 
-      expect(output.message).equals(notificationText);
-      expect(output.resource).equals(resource);
+        return chakram.put(Prepper.foxboxManager.setterURL,setterPayload)
+        .then(function(cmdResp){
+         expect(cmdResp).to.have.status(200);
+         // When there is no error, the payload returns null with the setter
+         expect(cmdResp.body[setter]).equals(null);
+         return chakram.put(Prepper.foxboxManager.getterURL,getterPayload);
+        })
+        .then(function(cmdResp){
+          expect(cmdResp).to.have.status(200);
+          expect(cmdResp.body[getter].Json.subscriptions[0].public_key)
+          .equals(Prepper.webPush_server.getPublicKey());
+          expect(cmdResp.body[getter].Json.subscriptions[0].push_uri)
+          .equals(Prepper.webPush_server.getEndpointURI());
+        });
+      });
+
+      it('Set resources to receive notification',function(){     
+        var getter =  'getter:resource.webpush@link.mozilla.org';   
+        var setter = 'setter:resource.webpush@link.mozilla.org';
+        var setterPayload = {
+          'select': {
+            'id': setter}, 
+            'value': {
+              'Json': {
+                'resources':['livingroom', 'washroom']}}};
+        var getterPayload = {'id': getter};
+
+        return chakram.put(Prepper.foxboxManager.setterURL,setterPayload)
+        .then(function(cmdResp){
+         expect(cmdResp).to.have.status(200);
+         expect(cmdResp.body[setter]).equals(null);
+         return chakram.put(Prepper.foxboxManager.getterURL,getterPayload);
+        })
+        .then(function(cmdResp){
+          expect(cmdResp).to.have.status(200);
+          expect(cmdResp.body[getter]
+            .Json.resources[0]).equals('livingroom');
+          expect(cmdResp.body[getter]
+            .Json.resources[1]).equals('washroom');
+        });  
+      });
+
+      it('Accepts notification trigger requests',function(){
+        var setter = 'setter:notify.webpush@link.mozilla.org';
+        var resource = 'washroom';
+        var notificationText = 'lights on!';
+        var payload = {
+          'select': {
+            'id': setter
+          }, 
+          'value': {
+            'WebPushNotify': {
+              'resource':resource,'message':notificationText}}} ;
+        return chakram.put(Prepper.foxboxManager.setterURL, payload)
+        .then(function(cmdResponse) {
+          expect(cmdResponse).to.have.status(200);
+          expect(cmdResponse.body[setter]).equals(null);
+        });
+      });
+
+      describe ('Once notification is sent', function () {
+        var resource = 'livingroom';
+        var notificationText = 'can you encrypt this one too?';
+
+        before(function(done) {
+          var setter = 'setter:notify.webpush@link.mozilla.org';
+          var payload = {
+            'select': {
+              'id': setter
+            }, 
+            'value': {
+              'WebPushNotify': {
+                'resource':resource,'message':notificationText}}} ;
+          return chakram.put(Prepper.foxboxManager.setterURL, payload)
+          .then(function(cmdResponse) {
+          // collect the response from the webpush simulator 
+          // after 100ms of (network)latency
+          setTimeout(done,100);
+          });
+        });
+
+        it('Check notification is received by Push server',function(){
+          var output = JSON.parse(Prepper.webPush_server.getDecodedPushMsg());
+
+          expect(output.message).equals(notificationText);
+          expect(output.resource).equals(resource);
+        });
+
+        it('Remove subscription', function(){
+          var setter = 'setter:unsubscribe.webpush@link.mozilla.org';
+          var getter =  'getter:subscription.webpush@link.mozilla.org';
+          var setterPayload = {
+            'select': {
+              'id':setter
+            }, 
+            'value': {
+              'Json': {
+                'subscriptions':[{
+                  'push_uri':pushURI,
+                  'public_key':pushkey
+                }]}}};
+          var getterPayload = {'id': getter};
+
+          return chakram.put(Prepper.foxboxManager.setterURL,setterPayload)
+          .then(function(cmdResp){
+           expect(cmdResp).to.have.status(200);
+           expect(cmdResp.body[setter]).equals(null);
+           return chakram.put(Prepper.foxboxManager.getterURL,getterPayload);
+          })
+          .then(function(cmdResp){
+            expect(cmdResp).to.have.status(200);
+            expect(cmdResp.body[getter].Json.subscriptions.length).equals(0);
+          });
+        });
+      });
     });
   });
 });
