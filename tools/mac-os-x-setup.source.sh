fbcda44b946d3e914363768ec8c1d8c238b83c1e
Johan Lorenzo <jlorenzo@mozilla.com>
2016-04-27 12:21:08 +0200

    fix nit

--- tools/mac-os-x-setup.source.sh
+++ tools/mac-os-x-setup.source.sh
@@ -1,7 +1,7 @@
 # This script must be sourced. Please run `source tools/mac-os-x-setup.sh`
 
 # openssl and sqlite libraries are already installed on OS X base system. That's
-# why we need to export them explicitely.
+# why we need explicitely specify the directories that brew installed.
 # TODO: Remove sqlite once pkg_config is able to not bubble up a system path
 # For more information, see https://github.com/fxbox/foxbox/issues/414
 export DEP_OPENSSL_INCLUDE="$(brew --prefix openssl)/include"
