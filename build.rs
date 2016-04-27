ecc92fae61141185046ceaa7131a53a44258d4b4
Julien Wajsberg <felash@gmail.com>
2016-04-27 16:35:51 +0200

    Move dns challenge as an additional binary for foxbox (Closes #423, #424)
    This also adds a run.sh script to easily run foxbox

--- build.rs
+++ build.rs
@@ -4,8 +4,7 @@
 
 use std::env;
 use std::fs;
-use std::path::{ Path, PathBuf };
-use std::process::Command;
+use std::path::Path;
 extern crate pkg_config;
 
 fn update_local_git_hook() {
@@ -50,59 +49,6 @@ fn copy_shared_static_files() {
     }
 }
 
-// Looks for an environmental variable, defaulting to the empty string.
-fn get_env(key: &str) -> String {
-    env::var(key).unwrap_or("".to_owned())
-}
-
-// Returns the appropriate --target=XXX command line argument, or None
-// if we are not cross compiling.
-fn get_target() -> Option<String> {
-    let target = get_env("TARGET");
-    let host = get_env("HOST");
-    if host != target {
-        return Some(format!("--target={}", target));
-    }
-
-    None
-}
-
-fn cargo_build_in_directory(directory: &str) {
-    let current_dir = env::current_dir().unwrap();
-    let mut run_in_dir = PathBuf::from(&current_dir);
-    run_in_dir.push(directory);
-
-    let mut exec = Command::new("cargo");
-    let mut command = exec.arg("build")
-                          .arg("--verbose"); // Ideally should reflect the --verbose flag of the main
-                                             // cargo command.
-
-    let target = get_target();
-    if target.is_some() {
-        let tmp = command;
-        command = tmp.arg(target.unwrap());
-    }
-
-    // $PROFILE is either `debug` (default value) or `release`.
-    if get_env("PROFILE") == "release" {
-        let tmp = command;
-        command = tmp.arg("--release");
-    }
-
-    command.current_dir(run_in_dir)
-           .spawn()
-           .unwrap()
-           .wait()
-           .unwrap();
-
-    /*if !output.status.success() {
-        println!("status: {}", output.status);
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
-        panic!("debug"); // Forces the println!(...) to display.
-    }*/
-}
-
 fn link_external_libs() {
     pkg_config::probe_library("libupnp").unwrap();
 }
@@ -111,5 +57,4 @@ fn main() {
     update_local_git_hook();
     link_external_libs();
     copy_shared_static_files();
-    cargo_build_in_directory("./components/dns_challenge");
 }
