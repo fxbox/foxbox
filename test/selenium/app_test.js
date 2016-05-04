fc9025579c583856027f76e141ec9582579e32ce
Johan Lorenzo <jlorenzo@mozilla.com>
2016-05-04 17:15:08 +0200

    Reenable github.io test

--- test/selenium/app_test.js
+++ test/selenium/app_test.js
@@ -3,18 +3,15 @@
 var Prepper = require('./lib/testPrepperSelenium.js');
 var webdriver = require('selenium-webdriver');
 
-var HOST_URL = 'https://fxbox.github.io/app';
+var HOST_URL = 'http://fxbox.github.io/app';
 
 var SetUpWebapp = require('./lib/setup_webapp.js');
 var webAppMainPage;
 var setUpWebapp;
 
-Prepper.makeSuite('Test to open web app', function() {
+Prepper.makeSuite('Github.io webapp', function() {
 
-  describe('UI app', function() {
   var driver;
-  this.timeout(10000);
-
   const PASSWORD = '12345678';
 
   before(function() {
@@ -31,19 +28,17 @@ Prepper.makeSuite('Test to open web app', function() {
     driver.quit();
   });
 
-  describe.skip('open the web app', function() {
+  describe('open the web app', function() {
 
     beforeEach(function() {
       setUpWebapp = new SetUpWebapp(driver);
       webAppMainPage = setUpWebapp.getAppMainView();
-      return webAppMainPage;
     });
 
     it('should log in from web app', function() {
       return webAppMainPage.connectToFoxBox().then((setUpView) => {
         setUpView.successSignUpFromApp(PASSWORD);
-        });
+      });
     });
   });
-});
 });
