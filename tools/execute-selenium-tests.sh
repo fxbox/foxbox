5341b27b8ba80986d1b8dcc9d73f0c93a2b52678
Johan Lorenzo <jlorenzo@mozilla.com>
2016-05-09 15:38:08 +0200

    Selenium tests: Keep same domain name by not deleting certs

--- tools/execute-selenium-tests.sh
+++ tools/execute-selenium-tests.sh
@@ -4,11 +4,6 @@ set -ex
 
 CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_HOME="$CURRENT_PATH/.."
-FOXBOX_BINARY="$PROJECT_HOME/target/debug/foxbox"
-DEBUG_PROFILE="$HOME/.local/share/foxbox-tests"
-
-# TODO: Put this part in NodeJS instead
-rm -rf "${DEBUG_PROFILE}"
 
 # build and launch foxbox daemon
 pushd "${PROJECT_HOME}"
