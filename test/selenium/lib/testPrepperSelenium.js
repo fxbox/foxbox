22c7d71de4b378105b29645e026f2737bd451e16
Johan Lorenzo <jlorenzo@mozilla.com>
2016-05-09 12:26:55 +0200

    Decrease selenium timeout tests to 30 seconds

--- test/selenium/lib/testPrepperSelenium.js
+++ test/selenium/lib/testPrepperSelenium.js
@@ -5,14 +5,13 @@ var foxboxManager = require('./foxboxHelperSelenium.js');
 var testPrepperSelenium = (function() {
 
   function beforeTest(done) {
-
     console.log('test started');
     foxboxManager.fullOptionStart(done);
   }
 
   function makeSuite(desc, test) {
     describe(desc, function () {
-      this.timeout(5000000);
+      this.timeout(30000);
       before(beforeTest);
       test();
       after(function() {
