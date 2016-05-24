b2f688a7c5e64a4e55b67765bcebfdc8fb2ab588
Johan Lorenzo <jlorenzo@mozilla.com>
2016-05-24 12:19:08 +0200

    only execute thinkerbell tests

--- tools/execute-all-rust-tests.sh
+++ tools/execute-all-rust-tests.sh
@@ -6,7 +6,7 @@ set -ex
 CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_PATH="$CURRENT_PATH/.."
 COMPONENTS_CRATES="$(find "$PROJECT_PATH/components/" -mindepth 1 -maxdepth 1 -type d)"
-CRATES="$PROJECT_PATH $COMPONENTS_CRATES"
+CRATES="$PROJECT_PATH/components/thinkerbell"
 
 for crate in $CRATES; do
     cd $crate
