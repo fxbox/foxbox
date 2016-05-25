cf4c5d70c0d5c4ad9808a17013cf7ec10e6c9d69
Johan Lorenzo <jlorenzo@mozilla.com>
2016-05-25 17:50:26 +0200

    deactivate other tests

--- tools/travis-linux.sh
+++ tools/travis-linux.sh
@@ -11,24 +11,11 @@ build() {
 }
 
 lint() {
-    jshint test/**/*.js \
-        static/main/js/*.js static/setup/js/*.js
-        # There is a minified js in static/**/shared which breaks jshint
+    echo 'nothing to do'
 }
 
-_set_up_unit_tests() {
-    sudo usermod -a -G netdev "$USER" # "netdev" group membership allows to set custom host name via avahi-daemon.
-}
-
-_set_up_selenium_tests() {
-    sh -e /etc/init.d/xvfb start
-    export DISPLAY=:99.0
-    wget http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar
-    java -jar selenium-server-standalone-2.53.0.jar > /dev/null &
-    sleep 5
-    nvm install 4.2
-    nvm use 4.2
-    npm install
+set_up_tests() {
+    echo 'nothing to do'
 }
 
 _e2e_tests() {
@@ -39,19 +26,6 @@ _e2e_tests() {
     gulp test-e2e
 }
 
-set_up_tests() {
-    _set_up_unit_tests
-    _set_up_selenium_tests
-    _e2e_tests
-}
-
 run_tests() {
-    npm run test-selenium
-    npm run test-integration-travis
-    # TODO: Currently unit tests are executed twice. We need a way to filter out
-    # tests with `cargo test` depending on where they are defined in the tree
-    "$CURRENT_PATH/execute-all-rust-tests.sh"
-    # Note: Cargo recompiles every dependency with dead code. That's why
-    # this step is currently the last
-    "$CURRENT_PATH/execute-unit-tests-with-coverage.sh"
+    _e2e_tests
 }
