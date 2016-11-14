2170530e2f474e8c3f94bdc73c849af42f125053
Fabrice Desré <fabrice@desre.org>
2016-11-13 20:21:43 -0800

    Don't open an http tunnel. (#563)

--- src/tunnel_controller.rs
+++ src/tunnel_controller.rs
@@ -96,8 +96,7 @@ impl TunnelConfig {
 
         let res = Command::new("pagekite")
                 .arg(format!("--frontend={}", format!("{}:{}", domain, port)))
-                // XXX remove http service once we support https
-                .arg(format!("--service_on=http,https:{}:localhost:{}:{}",
+                .arg(format!("--service_on=https:{}:localhost:{}:{}",
                              self.remote_name,
                              self.local_http_port,
                              self.tunnel_secret))
@@ -107,7 +106,7 @@ impl TunnelConfig {
                              self.tunnel_secret))
                 .spawn();
         if res.is_err() {
-            error!("Failed to launch pagekite.py, check that it's installed and in your $PATH.");
+            error!("Failed to launch pagekite, check that it's installed and in your $PATH.");
         }
         res
     }
