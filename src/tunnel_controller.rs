5d5e6d849f478f407e027c071f8728a5a22be520
Fabrice Desré <fabrice@desre.org>
2016-12-19 21:51:19 -0800

    Remove unused ParseError

--- src/tunnel_controller.rs
+++ src/tunnel_controller.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use url::{ParseError, Url};
+use url::Url;
 
 use pagekite::{PageKite, InitFlags, LOG_NORMAL};
 
